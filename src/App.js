6e6ae62bbd36f89abd8c672d39fdfdaf5999b7cd
kingdomlevel <nllmrrs@aol.com>
2021-02-07 16:54:29 +0000

    re-written to use tmdb api

--- src/App.js
+++ src/App.js
@@ -25,46 +25,55 @@ function App() {
 
   useEffect(() => {
     const filmPromises = filmNames.map(title => {
-      return fetch(`http://www.omdbapi.com/?t=${title}&apikey=${env.OMDB_API_KEY}`)
+      return fetch(`https://api.themoviedb.org/3/search/movie?query=${title}&api_key=${env.TMDB_API_KEY}`)
               .then(res => res.json())
     });
 
-    Promise.all(filmPromises).then(allFilmData => {
-      console.log('all film data:', allFilmData);
-      
-      // get actors from film data
-      const repeatingActors = allFilmData.map(film => film.Actors.split(', ')).flat();
-      const actorOccurences = getActorOccurences(repeatingActors);
-      console.log('actor occurences', actorOccurences);
-
-      // set chart data
-      const chartOptions = {
-        chart: {
-          type: 'bar'
-        },
-        title: {
-          text: 'Actor appearences'
+    Promise.all(filmPromises).then(filmSearchResults => {
+      console.log('all film data:', filmSearchResults);
+      const filmIds = filmSearchResults.map(searchRes => searchRes.results[0].id);
+      const filmDetailPromises = filmIds.map(id => {
+        return fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${env.TMDB_API_KEY}&language=en-US`)
+                  .then(res => res.json())
         }
-      };
+      );
 
-      const chartSeries = [];
-      for(let actorName in actorOccurences) {
-        const bar = {
-          name: actorName,
-          data: actorOccurences[actorName]
-        }
-        chartSeries.push(bar);
-      };
+      Promise.all(filmDetailPromises).then(filmDetailResults => {
+        debugger;
+        // get actors from film data
+        const repeatingActors = filmDetailResults.map(film => film.cast.map(actor => actor.name)).flat();
+        const actorOccurences = getActorOccurences(repeatingActors);
+        console.log('actor occurences', actorOccurences);
 
-      // sort descending
-      chartSeries.sort((actor1, actor2) => actor2.data - actor1.data);
+        // set chart data
+        const chartOptions = {
+          chart: {
+            type: 'bar'
+          },
+          title: {
+            text: 'Actor appearences'
+          }
+        };
 
+        const chartSeries = [];
+        for (let actorName in actorOccurences) {
+          const bar = {
+            name: actorName,
+            data: actorOccurences[actorName]
+          }
+          chartSeries.push(bar);
+        };
+
+        // sort descending
+        chartSeries.sort((actor1, actor2) => actor2.data - actor1.data);
+
+
+
+        chartOptions["series"] = chartSeries.slice(0, 15);
+
+        setChartOptions(chartOptions);
+      })
       
-
-      chartOptions["series"] = chartSeries.slice(0, 15);
-
-      setChartOptions(chartOptions);
-
     });
   }, [filmNames]);
 
