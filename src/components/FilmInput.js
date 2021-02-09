0ee9c994261b84255bda1f6e2a0384a7570b7182
kingdomlevel <nllmrrs@aol.com>
2021-02-09 17:13:35 +0000

    adds sample data

--- src/components/FilmInput.js
+++ src/components/FilmInput.js
@@ -1,4 +1,20 @@
 const FilmInput = ({setFilmNames}) => {
+  const defaultVals = `School of Rock
+Prisoners
+Whiplash
+Blues Brothers
+The Prestige
+The Fly
+Interview with the Vampire
+The Peanut Butter Falcon
+Die Hard
+O Brother Where Art Thou
+Wild Rose
+Do The Right Thing
+Mon Oncle
+Disobedience
+Pretty Woman`;
+
   const getFilms = ev => {
     ev.preventDefault();
 
@@ -21,7 +37,9 @@ const FilmInput = ({setFilmNames}) => {
         name="films-input"
         rows="16"
         cols="30"
-      />
+      >
+        {defaultVals}
+      </textarea>
       <button type="submit">get actors</button>
     </form>
   )
